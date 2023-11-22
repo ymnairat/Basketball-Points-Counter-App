3766b6dfb3c521c62b139fcc9bf2c0f8e1f6f150
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-22 13:57:57 +0200

    Add vertical divider

--- lib/main.dart
+++ lib/main.dart
@@ -16,58 +16,123 @@ class BasketballPointsCounterApp extends StatelessWidget {
           backgroundColor: Colors.orange,
           title: const Text('Points Counter'),
         ),
-        body: Column(
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            const Text(
-              'Team A',
-              style: TextStyle(
-                fontSize: 32,
-              ),
-            ),
-            const Text(
-              '0',
-              style: TextStyle(
-                fontSize: 150,
-              ),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orange,
-                  minimumSize: const Size(150, 50)),
-              onPressed: () {},
-              child: const Text(
-                'Add 1 Point',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text(
+                  'Team A',
+                  style: TextStyle(
+                    fontSize: 32,
+                  ),
                 ),
-              ),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orange,
-                  minimumSize: const Size(150, 50)),
-              onPressed: () {},
-              child: const Text(
-                'Add 1 Point',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
+                const Text(
+                  '0',
+                  style: TextStyle(
+                    fontSize: 150,
+                  ),
                 ),
-              ),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orange,
-                  minimumSize: const Size(150, 50)),
-              onPressed: () {},
-              child: const Text(
-                'Add 1 Point',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      minimumSize: const Size(150, 50)),
+                  onPressed: () {},
+                  child: const Text(
+                    'Add 1 Point',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
-              ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      minimumSize: const Size(150, 50)),
+                  onPressed: () {},
+                  child: const Text(
+                    'Add 1 Point',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      minimumSize: const Size(150, 50)),
+                  onPressed: () {},
+                  child: const Text(
+                    'Add 1 Point',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const VerticalDivider(
+              color: Colors.grey,
+              thickness: 1,
+            ),
+            Column(
+              children: [
+                const Text(
+                  'Team B',
+                  style: TextStyle(
+                    fontSize: 32,
+                  ),
+                ),
+                const Text(
+                  '0',
+                  style: TextStyle(
+                    fontSize: 150,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      minimumSize: const Size(150, 50)),
+                  onPressed: () {},
+                  child: const Text(
+                    'Add 1 Point',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      minimumSize: const Size(150, 50)),
+                  onPressed: () {},
+                  child: const Text(
+                    'Add 1 Point',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      minimumSize: const Size(150, 50)),
+                  onPressed: () {},
+                  child: const Text(
+                    'Add 1 Point',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
