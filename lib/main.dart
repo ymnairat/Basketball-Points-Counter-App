6f356ea059bbe90765e741054e34719165e3614b
Mahmoud Nairat <mahmoud.a.nairat@gmail.com>
2023-11-22 13:14:48 +0200

    Add text widget for team and 0

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const BasketballPointsCounterApp());
 }
+
 class BasketballPointsCounterApp extends StatelessWidget {
   const BasketballPointsCounterApp({super.key});
 
@@ -15,7 +16,23 @@ class BasketballPointsCounterApp extends StatelessWidget {
           backgroundColor: Colors.orange,
           title: const Text('Points Counter'),
         ),
-      )
+        body: const Column(
+          children: [
+            Text(
+              'Team A',
+              style: TextStyle(
+                fontSize: 32,
+              ),
+            ),
+            Text(
+              '0',
+              style: TextStyle(
+                fontSize: 150,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
